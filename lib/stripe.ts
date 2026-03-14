3f0ee3bb5e1c4bd3ba737345e88f640601d581be
zaxscape0 <info@zaxscape.com>
2026-03-14 06:54:48 -0400

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe'                                                                                                          
                                                                                                                                         
    export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {                                                         
-     apiVersion: '2024-12-18.acacia',                                                                                                   
+     apiVersion: '2026-02-25.clover',                                                                                                   
    })                                                                                                                                   
                                                                                                                                         
    export const PLANS: Record<string, { name: string; price: number; priceId: string; competitorLimit: number }> = {                    
