7a89f92a28be8376af7e959a4e7492f8e2b9e033
zaxscape0 <info@zaxscape.com>
2026-03-13 23:48:54 -0400

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe'                                                                                                          
                                                                                                                                         
-   export const stripe = new Stripe(process.env.STRIPE_SECRET _KEY as string, {                                                         
+   export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {                                                         
      apiVersion: '2024-12-18.acacia',                                                                                                   
    })                                                                                                                                   
                                                                                                                                         
